a44a11eb4018cedae7b266e8783a35a6245f2d4b
KaLong_desktop <kaxm462@gmail.com>
2023-03-30 00:21:06 +0200

    working code

---
+++ main.py
@@ -0,0 +1,23 @@
+import paho.mqtt.client as mqtt
+
+# Set up the MQTT broker and topic
+MQTT_BROKER = "test.mosquitto.org"
+MQTT_TOPIC = "example/topic"
+
+# Define a callback function to handle incoming messages
+def on_message(client, userdata, message):
+    print("Message received via MQTT:")
+    print(message.payload.decode('utf-8'))
+
+# Set up the MQTT client and connect to the broker
+client = mqtt.Client()
+client.connect(MQTT_BROKER, 1883)
+
+# Set up the callback function to be called when a message is received
+client.on_message = on_message
+
+# Subscribe to the MQTT topic
+client.subscribe(MQTT_TOPIC)
+
+# Start the MQTT client loop to listen for incoming messages
+client.loop_forever()
