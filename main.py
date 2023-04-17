d37bdf1a9840fe8ff864726a00eebad02e13dda2
Ka Long Mok <kaxm462@gmail.com>
2023-04-17 11:30:26 +0200

    17-04-2023 Final working code with realtime and pre-defined data. ( But Only 1 concept descirption)

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import uvicorn
+import requests
 
 
 #region logging
@@ -33,6 +34,7 @@ MQTT_BROKER = "test.mosquitto.org"
 MQTT_TOPIC = "example/topic"
 
 global_data = None
+global_data_pre = None
 last_log_time = 0
 update_event = threading.Event()
 data_lock = threading.Lock()
@@ -55,8 +57,270 @@ def get_data():
         else:
             raise HTTPException(status_code=404, detail="Data not available")
 
+
+class StandardData(BaseModel):
+    submodels: list
+@app.get("/standardData", response_model=StandardData)
+def get_data():
+    global global_data_pre
+    if global_data_pre is not None:
+        return JSONResponse(content=global_data_pre)
+    else:
+        raise HTTPException(status_code=404, detail="Data not available")
+
 #endregion
 
+#region set initial values
+
+def set_initial_values():
+    global global_data_pre
+
+    submodel_chiller_identification = aas_types.Submodel(
+        id="urn:zhaw:ims:chiller:543fsfds99342:identification",
+        submodel_elements=[
+            aas_types.Property(
+                value="Thermo-chiller",
+                id_short="name",
+                value_type=aas_types.DataTypeDefXsd.STRING
+            ),
+            aas_types.Property(
+                value="Circulating Fluid Temperature Controller Thermo chiller",
+                id_short="full_name",
+                value_type=aas_types.DataTypeDefXsd.STRING
+            ),
+            aas_types.Property(
+                value="yX488",
+                id_short="serial_no",
+                value_type=aas_types.DataTypeDefXsd.STRING
+            ),
+            aas_types.Property(
+                value="HRS050-AF-20",
+                id_short="model",
+                value_type=aas_types.DataTypeDefXsd.STRING
+            ),
+            aas_types.Property(
+                value="HRS",
+                id_short="series",
+                value_type=aas_types.DataTypeDefXsd.STRING
+            ),
+            aas_types.Property(
+                value="69",
+                id_short="weight_kg",
+                value_type=aas_types.DataTypeDefXsd.INT
+            ),
+            aas_types.Property(
+                value="976x377x592",
+                id_short="size_mm",
+                value_type=aas_types.DataTypeDefXsd.STRING
+            ),
+            aas_types.Property(
+                value="SMC (Japan)",
+                id_short="manufacturer",
+                value_type=aas_types.DataTypeDefXsd.STRING
+            ),
+            aas_types.Property(
+                value="HRX-OM-0020 HRX-OM-0021",
+                id_short="manual",
+                value_type=aas_types.DataTypeDefXsd.STRING
+            ),
+        ]
+    )
+
+    submodel_chiller_standard_technical_data = aas_types.Submodel(
+        id="urn:zhaw:ims:chiller:543fsfds99342:standard_technical_data",
+        submodel_elements=[
+            aas_types.Property(
+                value="2.5",
+                id_short="capacity_aic_ka",
+                value_type=aas_types.DataTypeDefXsd.FLOAT
+            ),
+            aas_types.Property(
+                value="30-70",
+                id_short="humidity_range_percent",
+                value_type=aas_types.DataTypeDefXsd.STRING
+            ),  # AAS.TYPES.Range should replace this property
+            aas_types.Property(
+                value="3000",
+                id_short="altitude_m",
+                value_type=aas_types.DataTypeDefXsd.INT
+            ),
+            aas_types.Property(
+                value="R410A (HFC)",
+                id_short="refrigerant",
+                value_type=aas_types.DataTypeDefXsd.STRING
+            ),  # Could link to another URL or website (aas_types.DataTypeDefXsd.AnyURI)
+            aas_types.Property(
+                value="0.65",
+                id_short="refrigerant_charge_kg",
+                value_type=aas_types.DataTypeDefXsd.FLOAT
+            ),
+            aas_types.Property(
+                value="PID control",
+                id_short="control_method",
+                value_type=aas_types.DataTypeDefXsd.STRING
+            ),
+            aas_types.Property(
+                value="65/68",
+                id_short="noise_level_50Hz_60Hz_dB",
+                value_type=aas_types.DataTypeDefXsd.STRING
+            )
+        ]
+    )
+
+    submodel_chiller_standard_circulating_fluid_system_data = aas_types.Submodel(
+        id="urn:zhaw:ims:chiller:543fsfds99342:standard_circulating_fluid_system_data",
+        submodel_elements=[
+            aas_types.Property(
+                value="Tap water",
+                id_short="circulating_fluid",
+                value_type=aas_types.DataTypeDefXsd.STRING
+            ),
+            aas_types.Property(
+                value="5-40",
+                id_short="set_temperature_range_celsius",
+                value_type=aas_types.DataTypeDefXsd.STRING
+            ),
+            aas_types.Property(
+                value="4700/5100",
+                id_short="cooling_capacity_50Hz_60Hz_watt",
+                value_type=aas_types.DataTypeDefXsd.STRING
+            ),  # Separate 50 / 60 Hz and convert string -> int
+            aas_types.Property(
+                value="1100/1400",
+                id_short="heating_capacity_50Hz_60Hz_watt",
+                value_type=aas_types.DataTypeDefXsd.STRING
+            ),  # The unit can be outside (remind Markco)
+            aas_types.Property(
+                value="0.1",
+                id_short="temperature_stability",
+                value_type=aas_types.DataTypeDefXsd.FLOAT
+            ),
+            aas_types.Property(
+                value="23 (0.24 MPa)/28 (0.32 MPa)",
+                id_short="pump_rated_flow_50Hz_60Hz_lmin",
+                value_type=aas_types.DataTypeDefXsd.STRING
+            ),
+            aas_types.Property(
+                value="31/42",
+                id_short="pump_max_flow_rate_50Hz_60Hz_lmin",
+                value_type=aas_types.DataTypeDefXsd.STRING
+            ),
+            aas_types.Property(
+                value="50",
+                id_short="pump_max_head_50Hz_60Hz_m",
+                value_type=aas_types.DataTypeDefXsd.STRING
+            ),
+            aas_types.Property(
+                value="550",
+                id_short="pump_output_W",
+                value_type=aas_types.DataTypeDefXsd.FLOAT
+            ),
+            aas_types.Property(
+                value="5",
+                id_short="tank_capacity_L",
+                value_type=aas_types.DataTypeDefXsd.FLOAT
+            ),
+            aas_types.Property(
+                value="RC1/2",
+                id_short="port_size",
+                value_type=aas_types.DataTypeDefXsd.STRING
+            )
+        ]
+    )
+
+    submodel_chiller_standard_electrical_system_data = aas_types.Submodel(
+        id="urn:zhaw:ims:chiller:543fsfds99342:standard_electrical_system_data",
+        submodel_elements=[
+            aas_types.Property(
+                value="200-230",
+                id_short="power_supply_50Hz_60Hz_vac",
+                value_type=aas_types.DataTypeDefXsd.STRING
+            ),
+            aas_types.Property(
+                value="20",
+                id_short="circuit_protector_a",
+                value_type=aas_types.DataTypeDefXsd.FLOAT
+            ),
+            aas_types.Property(
+                value="20",
+                id_short="applicable_earth_leakage_breaker_capacity_a",
+                value_type=aas_types.DataTypeDefXsd.FLOAT
+            ),
+            aas_types.Property(
+                value="8/11",
+                id_short="rated_operating_current_a",
+                value_type=aas_types.DataTypeDefXsd.STRING
+            ),
+            aas_types.Property(
+                value="1.7/2.2",
+                id_short="rated_power_consumption_50Hz_60Hz_kva",
+                value_type=aas_types.DataTypeDefXsd.STRING
+            )
+        ]
+    )
+
+    asset_information = aas_types.AssetInformation(
+        asset_kind=aas_types.AssetKind.TYPE
+    )
+
+    chiller = aas_types.AssetAdministrationShell(
+        id="urn:zhaw:ims:chiller:543fsfds99342",
+        asset_information=asset_information,
+        submodels=[
+            aas_types.Reference(
+                type=aas_types.ReferenceTypes.MODEL_REFERENCE,
+                keys=[
+                    aas_types.Key(
+                        type=aas_types.KeyTypes.SUBMODEL,
+                        value="urn:zhaw:ims:chiller:543fsfds99342:identification"
+                    )
+                ]
+            ),
+            aas_types.Reference(
+                type=aas_types.ReferenceTypes.MODEL_REFERENCE,
+                keys=[
+                    aas_types.Key(
+                        type=aas_types.KeyTypes.SUBMODEL,
+                        value="urn:zhaw:ims:chiller:543fsfds99342:standard_technical_data"
+                    )
+                ]
+            ),
+            aas_types.Reference(
+                type=aas_types.ReferenceTypes.MODEL_REFERENCE,
+                keys=[
+                    aas_types.Key(
+                        type=aas_types.KeyTypes.SUBMODEL,
+                        value="urn:zhaw:ims:chiller:543fsfds99342:standard_circulating_fluid_system_data"
+                    )
+                ]
+            ),
+            aas_types.Reference(
+                type=aas_types.ReferenceTypes.MODEL_REFERENCE,
+                keys=[
+                    aas_types.Key(
+                        type=aas_types.KeyTypes.SUBMODEL,
+                        value="urn:zhaw:ims:chiller:543fsfds99342:standard_electrical_system_data"
+                    )
+                ]
+            )
+        ]
+    )
+
+    environment = aas_types.Environment(
+        submodels=[
+                   submodel_chiller_identification,
+                   submodel_chiller_standard_technical_data,
+                   submodel_chiller_standard_circulating_fluid_system_data,
+                   submodel_chiller_standard_electrical_system_data
+                   ]
+    )
+
+    jsonable_pre = aas_jsonization.to_jsonable(environment)
+
+    global_data_pre = jsonable_pre
+
+
+#endregion
 
 #region on_message()
 def on_message(client, userdata, message):
@@ -90,6 +354,8 @@ def update_data():
     global global_data, global_jsonable
 
     logger.debug("Entering update_data() function")
+
+    set_initial_values()
 
     while True:
 
@@ -259,6 +525,7 @@ client.loop_start()
 time.sleep(3)
 
 if __name__ == "__main__":
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
 
